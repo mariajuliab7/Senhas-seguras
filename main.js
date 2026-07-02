dda8cde5b23cce09b41b8d13db10cafee16f26a1
mariajuliab7 <maria.juliane.bueno@escola.pr.gov.br>
2026-07-02 12:54:02 -0300

    gg

--- main.js
+++ main.js
@@ -35,13 +35,30 @@ for (i = 0; i < checkbox.length; i++) {
 const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
 const numeros = '0123456789';
-const simbolos = ' '
+const simbolos = '!@%*?';
+
 geraSenha();
 
 function geraSenha(){
-
+    let alfabeto = ' ';
+    if (checkbox[0].checked) {
+        alfabeto = alfabeto + letrasMaiusculas;
+    }
+    if (checkbox[1].checked) {
+        alfabeto = alfabeto + letrasMinusculas;
+    }
+    if (checkbox[2].checked) {
+        alfabeto = alfabeto + numeros;
+    }
+    if (checkbox[3].checked) {
+        alfabeto = alfabeto + simbolos;
+    }
+    console.log(alfabeto);
+    let senha = '';
+    for (let i = 0; i <tamanhoSenha; i ++) {
+        let numeroAleatorio = Math.random() * alfabeto.length;
+        numeroAleatorio = Math.floor (numeroAleatorio);
+        senha = senha + alfabeto(numeroAleatorio);
+    }
+    campoSenha.value = senha;
 }
-
-
-
-campoSenha.value = letrasMaiusculas;
